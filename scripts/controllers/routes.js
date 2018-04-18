3b856d6a6aa6871790c273dd52431b0d348225bc
JB Tellez <jb.tellez@gmail.com>
2018-04-17 23:59:58 -0700

    spa-gh-pages tweak

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,7 +1,6 @@
 // NOTE: these routes will work just fine running locally
 // But when you start remotely check out routes-remote.js
 
-// page.base('/internets-of-thing');
 // all route changes should hide page containers
 page('/*', (ctx, next) => {
     $('.page').hide()
